0a06a56974228208eee36324ea71bab33a6340d1
tmkim1 <xoalstjtm@gmail.com>
2021-09-07 22:46:40 +0900

    Post Entity, Repository 추가 및 저장 테스트

---
+++ src/test/java/com/jojoidu/book/springboot/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.jojoidu.book.springboot.domain.posts;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class
+)
+@SpringBootTest
+public class PostRepositoryTest extends TestCase {
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Post.builder()
+                .title(title)
+                .content(content)
+                .author("xoalstjtm@gmail.com")
+                .build());
+
+        //when
+        List<Post> posts = postRepository.findAll();
+
+        //then
+        Post post = posts.get(0);
+        assertThat(post.getTitle()).isEqualTo(title);
+        assertThat(post.getContent()).isEqualTo(content);
+    }
+}

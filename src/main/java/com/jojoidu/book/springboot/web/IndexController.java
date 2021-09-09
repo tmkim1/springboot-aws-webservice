e9ee5c1f9f6699056e3540b192b7674942d60c2e
tmkim1 <xoalstjtm@gmail.com>
2021-09-09 23:44:27 +0900

    글 등록(POST: api/v1/post) 관련 mustache, js 추가

--- src/main/java/com/jojoidu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoidu/book/springboot/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.jojoidu.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/post/save")
+    public String postSave() {
+        return "post-save";
     }
 }
